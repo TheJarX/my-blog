4a81148395dc2ad9e132be01d7dac1544e9e75e9
Gerard Altamirano <galtamiranor@outlook.es>
2020-09-04 07:29:30 -0500

    Playing with config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,12 @@
 require(`dotenv`).config({
   path: `.env`,
-})
+});
 
-const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE
+const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE;
 
 module.exports = {
   siteMetadata: {
-    siteTitleAlt: `Minimal Blog - Gatsby Theme`,
+    siteTitleAlt: `Gerard Altamirano Blog`,
   },
   plugins: [
     {
@@ -26,11 +26,11 @@ module.exports = {
         externalLinks: [
           {
             name: `Twitter`,
-            url: `https://twitter.com/lekoarts_de`,
+            url: `https://twitter.com/galtamiranor`,
           },
           {
-            name: `Instagram`,
-            url: `https://www.instagram.com/lekoarts.de/`,
+            name: `Linkedin`,
+            url: `https://www.linkedin.com/in/gerardaltamirano/`,
           },
         ],
       },
@@ -77,4 +77,4 @@ module.exports = {
       },
     },
   ].filter(Boolean),
-}
+};
